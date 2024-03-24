2654d8bdb61ac03fdc26f032fc61623630fae324
Rustagi <prrustagi@deloitte.com>
2024-03-24 22:38:50 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./components/homepage/home";
+import HomeDashboard from "./components/dashboard/homeDashboard"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      <Routes path="/" >
+        <Route index element={<Home />} />
+      </Routes>
+      <Routes path="dashboard">
+        <Route path="" element={<HomeDashboard />} />
+      </Routes>
+    </Router>
     </div>
   );
 }
---
+++ src/components/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import {getFirestore} from "@firebase/firestore";
+import {getAuth} from "firebase/auth"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDAcMhoip08KKN71qlumHtaBr5PdSFKgDs",
+  authDomain: "hmsportal-a4d86.firebaseapp.com",
+  projectId: "hmsportal-a4d86",
+  storageBucket: "hmsportal-a4d86.appspot.com",
+  messagingSenderId: "181951849726",
+  appId: "1:181951849726:web:5367adcb5663cde70931af"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const firestore = getFirestore(app);
+export const auth = getAuth(app);
